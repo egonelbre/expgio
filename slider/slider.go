fa1ded03affeb190fb8ee0dd7bd99f64e63cd987
Egon Elbre <egonelbre@gmail.com>
2020-05-13 11:04:00 +0300

    slider: add smoothing

--- slider/slider.go
+++ slider/slider.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math"
 	"time"
 
 	"gioui.org/f32"
@@ -8,7 +9,7 @@ import (
 	"gioui.org/op"
 )
 
-const defaultDuration = 250 * time.Millisecond
+const defaultDuration = 300 * time.Millisecond
 
 type Slider struct {
 	Duration time.Duration
@@ -22,27 +23,27 @@ type Slider struct {
 	offset float32
 }
 
-func (slider *Slider) PushLeft(gtx *layout.Context) {
-	slider.push = 1
+func (s *Slider) PushLeft(gtx *layout.Context) {
+	s.push = 1
 }
 
-func (slider *Slider) PushRight(gtx *layout.Context) {
-	slider.push = -1
+func (s *Slider) PushRight(gtx *layout.Context) {
+	s.push = -1
 }
 
 func (s *Slider) Layout(gtx *layout.Context, w layout.Widget) {
 	if s.push != 0 {
-		slider.last, slider.next = slider.next, new(op.Ops)
-		slider.offset = float32(s.push)
-		slider.t0 = gtx.Now()
-		slider.push = 0
+		s.last, s.next = s.next, new(op.Ops)
+		s.offset = float32(s.push)
+		s.t0 = gtx.Now()
+		s.push = 0
 	}
 
 	var delta time.Duration
 	if !s.t0.IsZero() {
 		now := gtx.Now()
-		delta = now.Sub(slider.t0)
-		slider.t0 = now
+		delta = now.Sub(s.t0)
+		s.t0 = now
 	}
 
 	if s.offset != 0 {
@@ -77,7 +78,7 @@ func (s *Slider) Layout(gtx *layout.Context, w layout.Widget) {
 		gtx.Ops = prev
 	}
 
-	if slider.offset == 0 {
+	if s.offset == 0 {
 		op.CallOp{Ops: s.next}.Add(gtx.Ops)
 		return
 	}
@@ -86,9 +87,11 @@ func (s *Slider) Layout(gtx *layout.Context, w layout.Widget) {
 	stack.Push(gtx.Ops)
 	defer stack.Pop()
 
-	if slider.offset > 0 {
+	offset := absfn(s.offset, easeInOutCubic)
+
+	if s.offset > 0 {
 		op.TransformOp{}.Offset(f32.Point{
-			X: float32(gtx.Dimensions.Size.X) * (s.offset - 1),
+			X: float32(gtx.Dimensions.Size.X) * (offset - 1),
 		}).Add(gtx.Ops)
 		op.CallOp{Ops: s.last}.Add(gtx.Ops)
 
@@ -98,7 +101,7 @@ func (s *Slider) Layout(gtx *layout.Context, w layout.Widget) {
 		op.CallOp{Ops: s.next}.Add(gtx.Ops)
 	} else {
 		op.TransformOp{}.Offset(f32.Point{
-			X: float32(gtx.Dimensions.Size.X) * (s.offset + 1),
+			X: float32(gtx.Dimensions.Size.X) * (offset + 1),
 		}).Add(gtx.Ops)
 		op.CallOp{Ops: s.last}.Add(gtx.Ops)
 
@@ -108,3 +111,24 @@ func (s *Slider) Layout(gtx *layout.Context, w layout.Widget) {
 		op.CallOp{Ops: s.next}.Add(gtx.Ops)
 	}
 }
+
+func absfn(t float32, fn func(float32) float32) float32 {
+	if t < 0 {
+		return -fn(-t)
+	}
+	return fn(t)
+}
+
+func easeInOutCubic(t float32) float32 {
+	if t < 0.5 {
+		return 4 * t * t * t
+	}
+	return (t-1)*(2*t-2)*(2*t-2) + 1
+}
+
+func hesitant(t float32) float32 {
+	s := float32(math.Sin(float64(t * math.Pi * 2)))
+	return t + s*s*s/2
+}
+
+// return 0.0005 * (t - 1) * (t - 1) * t * (44851 - 224256 * t + 224256 * t * t)
