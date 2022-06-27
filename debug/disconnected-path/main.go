c1283a52652a9375482ec212d1413b5853fb7fdb
Egon Elbre <egonelbre@gmail.com>
2022-06-27 11:43:27 +0300

    debug: add few debug examples

---
+++ debug/disconnected-path/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"image/color"
+
+	"gioui.org/app"
+	"gioui.org/f32"
+	"gioui.org/io/profile"
+	"gioui.org/io/system"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/op/clip"
+	"gioui.org/op/paint"
+)
+
+func main() {
+	go func() {
+		w := app.NewWindow(
+			app.Title("Circles"),
+		)
+
+		var ops op.Ops
+
+		for e := range w.Events() {
+			switch e := e.(type) {
+			case system.FrameEvent:
+				gtx := layout.NewContext(&ops, e)
+
+				var p clip.Path
+				p.Begin(gtx.Ops)
+				for x := 0; x < 1000; x += 10 {
+					for y := 0; y < 1000; y += 10 {
+						p.MoveTo(f32.Pt(float32(x), float32(y)))
+						p.LineTo(f32.Pt(float32(x+5), float32(y)))
+						p.LineTo(f32.Pt(float32(x+5), float32(y+5)))
+						p.LineTo(f32.Pt(float32(x), float32(y+5)))
+						p.Close()
+					}
+				}
+				paint.FillShape(gtx.Ops, color.NRGBA{A: 0xFF}, clip.Outline{Path: p.End()}.Op())
+
+				for _, ev := range gtx.Events(0) {
+					fmt.Println(ev)
+				}
+				profile.Op{Tag: 0}.Add(gtx.Ops)
+				op.InvalidateOp{}.Add(gtx.Ops)
+
+				e.Frame(gtx.Ops)
+			}
+		}
+	}()
+
+	app.Main()
+}
---
+++ debug/rects/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+
+	"gioui.org/app"
+	"gioui.org/io/profile"
+	"gioui.org/io/system"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/op/clip"
+	"gioui.org/op/paint"
+)
+
+func main() {
+	go func() {
+		w := app.NewWindow()
+		var ops op.Ops
+
+		colors := []color.NRGBA{
+			{0xFF, 0, 0, 0xFF},
+			{0xFF, 0xFF, 0, 0xFF},
+			{0xFF, 0, 0xFF, 0xFF},
+			{0, 0xFF, 0xFF, 0xFF},
+			{0xAA, 0xFF, 0xFF, 0xFF},
+		}
+
+		profileTag := new(int)
+		for e := range w.Events() {
+			if ev, ok := e.(system.FrameEvent); ok {
+				gtx := layout.NewContext(&ops, ev)
+
+				for x := 0; x < 70; x++ {
+					for y := 0; y < 70; y++ {
+						stack := clip.Rect{
+							Min: image.Point{X: x * 10, Y: y * 10},
+							Max: image.Point{X: x*10 + 10, Y: y*10 + 10},
+						}.Push(gtx.Ops)
+						paint.Fill(gtx.Ops, colors[x*y%len(colors)])
+						stack.Pop()
+					}
+				}
+
+				for _, ev := range gtx.Events(profileTag) {
+					fmt.Println(ev)
+				}
+
+				profile.Op{Tag: profileTag}.Add(gtx.Ops)
+				op.InvalidateOp{}.Add(gtx.Ops)
+				ev.Frame(gtx.Ops)
+			}
+		}
+
+	}()
+
+	app.Main()
+}
