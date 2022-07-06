2b3debe5a671eb3973eec7aa2651a130216f6702
Egon Elbre <egonelbre@gmail.com>
2022-07-06 17:34:15 +0300

    debug/label: add example issue

---
+++ debug/label/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"gioui.org/app"
+	"gioui.org/font/gofont"
+	"gioui.org/io/system"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/unit"
+	"gioui.org/widget"
+	"gioui.org/widget/material"
+	"gioui.org/x/component"
+)
+
+func main() {
+	go func() {
+		w := app.NewWindow(
+			app.Title("Drawing Test"),
+			app.Size(unit.Dp(400), unit.Dp(600)),
+		)
+		if err := run(w); err != nil {
+			log.Fatal(err)
+		}
+		os.Exit(0)
+	}()
+	app.Main()
+}
+
+func run(w *app.Window) error {
+	th := material.NewTheme(gofont.Collection())
+	var list widget.List
+	list.Axis = layout.Vertical
+	ops := new(op.Ops)
+
+	for e := range w.Events() {
+		switch e := e.(type) {
+		case system.FrameEvent:
+			gtx := layout.NewContext(ops, e)
+
+			material.List(th, &list).Layout(gtx, 2,
+				func(gtx layout.Context, index int) layout.Dimensions {
+					switch index {
+					case 0:
+						style := material.Label(th, 14, "gioui.org/reallylongnamethatshouldntbewrappedunderanycondition")
+						style.MaxLines = 1
+						return style.Layout(gtx)
+					case 1:
+						style := material.Label(th, 14, "gioui.org/reallylongnamethatshouldntbewrappedunderanycondition")
+						style.MaxLines = 1
+						return component.TruncatingLabelStyle(style).Layout(gtx)
+					}
+					return layout.Dimensions{}
+				})
+
+			e.Frame(gtx.Ops)
+
+		case system.DestroyEvent:
+			return e.Err
+		}
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,14 +1,24 @@
 module github.com/egonelbre/expgio
 
-go 1.14
+go 1.17
 
 require (
 	gioui.org v0.0.0-20220607102828-5cf916c0753c
-	github.com/fogleman/gg v1.3.0 // indirect
+	gioui.org/x v0.0.0-20220609132952-25f67b5b7e87
 	github.com/fogleman/ln v0.0.0-20170223135521-12e6c6e74459
-	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/loov/hrtime v1.0.3
 	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d
 )
 
-replace gioui.org => ../gioui.org
+require (
+	gioui.org/cpu v0.0.0-20210817075930-8d6a761490d2 // indirect
+	gioui.org/shader v1.0.6 // indirect
+	github.com/benoitkugler/textlayout v0.1.1 // indirect
+	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/gioui/uax v0.2.1-0.20220325163150-e3d987515a12 // indirect
+	github.com/go-text/typesetting v0.0.0-20220411150340-35994bc27a7b // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	golang.org/x/exp v0.0.0-20210722180016-6781d3edade3 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
