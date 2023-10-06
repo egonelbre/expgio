a2fe7e993c95c7d634961b36c6d44df313705e9d
Egon Elbre <egonelbre@gmail.com>
2023-10-06 23:28:11 +0300

    scrollfade: quick proof of concept

---
+++ scrollfade/main.go
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: Unlicense OR MIT
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"gioui.org/app"
+	"gioui.org/io/system"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/widget"
+	"gioui.org/widget/material"
+)
+
+func main() {
+	ui := NewUI()
+
+	go func() {
+		w := app.NewWindow(
+			app.Title("Loader"),
+		)
+		if err := ui.Run(w); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		os.Exit(0)
+	}()
+	app.Main()
+}
+
+type UI struct {
+	theme *material.Theme
+	list  widget.List
+}
+
+func NewUI() *UI {
+	ui := &UI{
+		theme: material.NewTheme(),
+	}
+	ui.list.Axis = layout.Vertical
+	return ui
+}
+
+// Run handles window events and renders the application.
+func (ui *UI) Run(w *app.Window) error {
+	var ops op.Ops
+	for {
+		select {
+		case e := <-w.Events():
+			switch e := e.(type) {
+			case system.DestroyEvent:
+				return e.Err
+			case system.FrameEvent:
+
+				gtx := layout.NewContext(&ops, e)
+
+				pos := &ui.list.List.Position
+
+				// TODO: you need to take into account pos.Offset and pos.OffsetLast as well
+				// also first draw is invalid atm
+				center := pos.First + pos.Count/2
+
+				material.List(ui.theme, &ui.list).Layout(gtx, len(linesOfText),
+					func(gtx layout.Context, index int) layout.Dimensions {
+						s := fmt.Sprintf("%s [%#v]", linesOfText[index], ui.list.List.Position)
+						text := material.Body1(ui.theme, s)
+
+						distance := index - center
+						if distance < 0 {
+							distance = -distance
+						}
+						distance -= 3
+						if distance > 0 {
+							text.Color.A = uint8(max(255-distance*10, 0))
+						}
+
+						return text.Layout(gtx)
+					})
+
+				e.Frame(gtx.Ops)
+			}
+		}
+	}
+}
+
+var linesOfText = (func() []string {
+	xs := make([]string, 100)
+	for i := range xs {
+		xs[i] = strconv.Itoa(i)
+	}
+	return xs
+})()
