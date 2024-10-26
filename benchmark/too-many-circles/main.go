ec39e06bb6e876536c41d5fe88e1cd8100e788b0
Egon Elbre <egonelbre@gmail.com>
2024-10-26 14:29:46 +0300

    go.mod: bump gioui.org

---
+++ benchmark/too-many-circles/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"image"
+	"image/color"
+	"log"
+	"os"
+	"runtime/pprof"
+
+	"gioui.org/app"
+	"gioui.org/op"
+	"gioui.org/op/clip"
+	"gioui.org/op/paint"
+)
+
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+
+func main() {
+	flag.Parse()
+	go func() {
+		w := &app.Window{}
+		if err := loop(w); err != nil {
+			log.Println(err)
+		}
+	}()
+	app.Main()
+}
+
+func loop(w *app.Window) error {
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close()
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
+	var ops op.Ops
+	for {
+		switch e := w.Event().(type) {
+		case app.DestroyEvent:
+			return e.Err
+		case app.FrameEvent:
+			gtx := app.NewContext(&ops, e)
+
+			gtx.Execute(op.InvalidateCmd{})
+
+			const size = 10
+			count := 0
+			for y := 0; y < e.Size.Y; y += size {
+				for x := 0; x < e.Size.X; x += size {
+					paint.FillShape(gtx.Ops, color.NRGBA{
+						R: byte(x),
+						G: byte(y),
+						B: byte(x * y),
+						A: 0xFF,
+					}, clip.Ellipse{
+						Min: image.Point{X: x, Y: y},
+						Max: image.Point{X: x + size, Y: y + size},
+					}.Op(gtx.Ops))
+					count++
+				}
+			}
+
+			fmt.Println(count)
+
+			e.Frame(gtx.Ops)
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -3,23 +3,23 @@ module github.com/egonelbre/expgio
 go 1.21
 
 require (
-	gioui.org v0.6.0
+	gioui.org v0.7.1-0.20241025200657-8daff13af6cd
 	github.com/aquilax/go-perlin v1.1.0
 	github.com/fogleman/ln v0.0.0-20170223135521-12e6c6e74459
 	github.com/loov/hrtime v1.0.3
-	golang.org/x/image v0.7.0
+	golang.org/x/image v0.18.0
 )
 
 require (
 	gioui.org/cpu v0.0.0-20210817075930-8d6a761490d2 // indirect
 	gioui.org/shader v1.0.8 // indirect
 	github.com/fogleman/gg v1.3.0 // indirect
-	github.com/go-text/typesetting v0.1.1 // indirect
+	github.com/go-text/typesetting v0.1.2 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
-	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
-	golang.org/x/exp/shiny v0.0.0-20220827204233-334a2380cb91 // indirect
-	golang.org/x/sys v0.12.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/exp v0.0.0-20240707233637-46b078467d37 // indirect
+	golang.org/x/exp/shiny v0.0.0-20240707233637-46b078467d37 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
 )
 
 replace gioui.org => ../gioui.org
